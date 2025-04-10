4bac389d202b247a68e18accf3e8836b58299c8f
JK-INSANO <144865199+JK-INSANO@users.noreply.github.com>
2025-04-10 17:05:00 -0400

    added: logout testing page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,35 @@
+"use client"
+import { logout } from "@/services/api/auth" // Importa la función logout
+import { useRouter } from "next/navigation" // Importa useRouter para redirigir
+
 export default function Menu() {
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    const refreshToken = localStorage.getItem("token") // Obtén el refresh token
+    if (refreshToken) {
+      const success = await logout(refreshToken)
+      if (success) {
+        router.push("/") // Redirige al usuario a la página principal después de cerrar sesión
+      } else {
+        console.error("Error al cerrar sesión")
+      }
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4">
         <h1 className="text-4xl font-bold">
           <span>Register</span>
         </h1>
-        hola ya ingresaste
+        <p>hola ya ingresaste</p>
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+        >
+          Cerrar sesión
+        </button>
       </div>
     </main>
   )
