3bed70151472189e6e25ccb3471a1ef6fa45968d
Rubén Rojas Castro <144865199+JK-INSANO@users.noreply.github.com>
2025-04-13 22:41:38 -0400

    added page for login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./_LoginForm"
+
+export default function ForgotPassword() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center py-12">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4">
+        <LoginForm />
+      </div>
+    </main>
+  )
+}
