1d0e5334b899f9bca91121296b977344a99f037c
Rubén Rojas Castro <144865199+JK-INSANO@users.noreply.github.com>
2025-06-30 20:39:31 -0400

    b

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  skipTrailingSlashRedirect: true,
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
